2fdec5a332d1848a6a85ec028d51e006b2c3b71e
Bruno Tota <btota-ext@tendaatacado.com.br>
2024-03-03 21:50:18 -0300

    dev - melhorias tests

--- integration-tests/jest.setup.js
+++ integration-tests/jest.setup.js
@@ -1,4 +1,5 @@
-global.BASE_URL = 'localhost:8080'
-// global.BASE_URL = 'http://186.202.57.225:8080'
+const BASE_URL = process.env.local ? 'localhost:8080' : 'http://186.202.57.225:8080';
+
+global.BASE_URL = BASE_URL;
 global.TEST_EMAIL = 'automated_test@gmail.com';
 global.TEST_PASSWORD = 'E!ZGiNA29$zmnqH$';
